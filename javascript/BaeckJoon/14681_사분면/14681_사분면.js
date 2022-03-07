68c8582ea90a5372d2f24fdc798d453169b8675e
hk0920 <gmlrb920@naver.com>
2022-03-07 17:17:17 +0900

    14681_사분면 fs 런타임에러 발생=> 해결

--- javascript/BaeckJoon/14681_사분면/14681_사분면.js
+++ javascript/BaeckJoon/14681_사분면/14681_사분면.js
@@ -1,9 +1,34 @@
 const fs = require('fs');
 const inputData = fs.readFileSync('javascript/BaeckJoon/14681_사분면/example.txt').toString().split("\n");
 
-// for(let i=0; i<inputData.length; i++){
-// 	const A = inputData[i].split(" ")[0];
-// 	const B = inputData[i].split(" ")[1];
+for(let i=0; i<inputData.length; i++){
+	const A = inputData[i].split(" ")[0];
+	const B = inputData[i].split(" ")[1];
+
+	if(A > 0 && B > 0){
+		console.log(1);
+	}else if(A < 0 && B > 0){
+		console.log(2);
+	}else if(A < 0 && B < 0){
+		console.log(3);
+	}else if(A > 0 && B < 0){
+		console.log(4);
+	}
+}
+
+// 백준 fs로 사용하면 런 에러발생 => 해결
+// const readline = require('readline');
+// const rl = readline.createInterface({
+// 	input: process.stdin,
+// 	output : process.stdout
+// });   
+
+// let input = [];
+// rl.on("line", function(line){
+// 	input.push(parseInt(line));
+// }).on("close", function(){
+// 	const A = input[0];
+// 	const B = input[1];
 
 // 	if(A > 0 && B > 0){
 // 		console.log(1);
@@ -14,20 +39,4 @@ const inputData = fs.readFileSync('javascript/BaeckJoon/14681_사분면/example.
 // 	}else if(A > 0 && B < 0){
 // 		console.log(4);
 // 	}
-// }
-
-// const fs = require('fs');
-// const inputData = fs.readFileSync('dev/stdin').toString().split('\n');
-
-const A = inputData[0];
-const B = inputData[1];
-
-if(A > 0 && B > 0){
-    console.log(1);
-}else if(A < 0 && B > 0){
-    console.log(2);
-}else if(A < 0 && B < 0){
-    console.log(3);
-}else if(A > 0 && B < 0){
-    console.log(4);
-}
+// });
