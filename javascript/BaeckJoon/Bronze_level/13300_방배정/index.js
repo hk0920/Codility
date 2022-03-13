1f7a8da254e3d68d0a42be25ac7e1bf969419718
hk0920 <gmlrb920@naver.com>
2022-03-13 22:40:39 +0900

    baekjoon multiArray test file upload

--- javascript/BaeckJoon/Bronze_level/13300_방배정/index.js
+++ javascript/BaeckJoon/Bronze_level/13300_방배정/index.js
@@ -3,7 +3,41 @@ const inputData = fs.readFileSync('javascript/BaeckJoon/Bronze_level/13300_ë°©ë°
 
 function solution(arr, X){
 	let result = 0;
-	
+	const multiArr = [
+		[0,0],
+		[0,0],
+		[0,0],
+		[0,0],
+		[0,0],
+		[0,0]
+	];
+
+	for(let i=0; i<multiArr.length; i++){
+		let girl = 0;
+		let boy = 0;
+		for(let j=0; j<arr.length; j++){
+			if(arr[j].level === (i+1)){
+				if(arr[j].gender === 0){
+					girl += 1;
+				}else if(arr[j].gender === 1){
+					boy += 1;
+				}
+				multiArr[i] = [girl, boy];
+			}
+		}
+	}
+	console.log(multiArr)
+
+	// console.log(multiArr[1][0] / X)
+	for(let i=0; i<multiArr.length; i++){
+		for(let j=0; j<2; j++){
+			result += multiArr[i][j] / X;
+
+			if(multiArr[i][j] % X !== 0){
+				result += 1;
+			}
+		}
+	}
 
 	return result;
 }
