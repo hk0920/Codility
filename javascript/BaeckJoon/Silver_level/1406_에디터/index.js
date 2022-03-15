b50dc45ba103cf24ecaf1b640777e4603eb931c8
hk0920 <gmlrb920@naver.com>
2022-03-15 14:37:13 +0900

    baekjoon linked list test file upload

--- javascript/BaeckJoon/Silver_level/1406_에디터/index.js
+++ javascript/BaeckJoon/Silver_level/1406_에디터/index.js
@@ -4,26 +4,37 @@ const inputData = fs.readFileSync('javascript/BaeckJoon/Silver_level/1406_에디
 function solution(str, N, arr){
 	const strArr = str.trim().split("");
 
-	let idx = strArr.length+1;
+	let idx = strArr.length;
 	for(let i=0; i<N; i++){
 		const editorKey = arr[i].split(" ")[0];
 		const editorText = arr[i].split(" ")[1];
 
 		if(editorKey === "L"){
+			if(!idx) continue;
 			idx--;
 		}else if(editorKey === "D"){
+			if(idx === strArr.length) continue;
 			idx++;
 		}else if(editorKey === "B"){
+			if(!idx) continue;
 			if(editorText !== undefined){
 				strArr.splice(idx-1, 1);
 			}
+			idx--;
 		}else if(editorKey === "P"){
 			if(editorText !== undefined){
 				strArr.splice(idx, 0, editorText);
 			}
+			idx++;
 		}
 	}
 	console.log(strArr)
+	let result = "";
+	for(let i=0; i<strArr.length; i++){
+		result += strArr[i];
+	}
+
+	return result;
 }
 
 const initText = inputData[0];
