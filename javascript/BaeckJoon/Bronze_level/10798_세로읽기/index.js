ed5b29473b6acab80891abeae8b58d7944ae7cad
hk0920 <gmlrb920@naver.com>
2023-08-21 00:27:35 +0900

    [modify] 10798_로컬 ok / 백준 런타임에러

--- javascript/BaeckJoon/Bronze_level/10798_세로읽기/index.js
+++ javascript/BaeckJoon/Bronze_level/10798_세로읽기/index.js
@@ -1,19 +1,22 @@
 const fs = require("fs");
-const inputData = fs.readFileSync("./example.txt").toString().trim().split("\n");
+const inputData = fs.readFileSync("./example2.txt").toString().trim().split("\n");
 
 function solution(data){
 	let result = "";
+	let max = 0;
 
 	for(let i=0; i<data.length; i++){
-		const word = data[i].trim().split("");
-		data[i] = word;
-		// console.log(word)
-		for(let j=0; j<word.length; j++){
-			// console.log("?", data[j][i])
-			result += data[j][i] || "";
+		max = data[i].trim().split("").length;
+		data[i] = data[i].trim().split("");
+	}
+	
+	for(let i=0; i<max; i++){
+		for(let j=0; j<data.length; j++){
+			if(data[j][i] !== undefined){
+				result += data[j][i]	
+			}
 		}
 	}
-	// console.log(data)
 
 	return result;
 }
